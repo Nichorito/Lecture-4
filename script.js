c6fd6d7387ee8606ebfc738441b23114a3d2b6c1
Nichorito <nsheppard999@gmail.com>
2025-01-28 15:12:40 -0500

    Complete site

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("submitButton").addEventListener("click", function () {
+    const numberInput = document.getElementById("number").value;
+  
+    if (parseInt(numberInput, 10) === 42) {
+      alert("YOU WON!  CONGRATULATIONS");
+      window.open("https://www.instagram.com/nichorito/", "_blank");
+    } else {
+      alert("Sorry you lost the game");
+      window.open("https://pornhub.com/midget", "_blank");
+    }
+  });
